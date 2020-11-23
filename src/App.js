3681ef9504f8c9052d4404fd95a151f0c13851ad
Yulian Borisov <julian.borisov0@gmail.com>
2020-11-24 00:46:55 +0200

    initial, todo: add styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+const ffmpeg = createFFmpeg({ log: true });
+
+export default function App() {
+  const [ready, setReady] = useState(false);
+  const [video, setVideo] = useState();
+  const [gif, setGif] = useState();
+
+  const load = async () => {
+    await ffmpeg.load();
+    setReady(true);
+  };
+
+  const convertToGif = async () => {
+    ffmpeg.FS('writeFile', 'upload.mp4', await fetchFile(video));
+
+    await ffmpeg.run(
+      '-i',
+      'upload.mp4',
+      '-t',
+      '5.0',
+      '-ss',
+      '2.0',
+      '-f',
+      'gif',
+      'out.gif'
+    );
+
+    const data = ffmpeg.FS('readFile', 'out.gif');
+
+    const url = URL.createObjectURL(
+      new Blob([data.buffer], { type: 'image/gif' })
+    );
+    setGif(url);
+  };
+
+  useEffect(() => {
+    load();
+  }, []);
+
+  return ready ? (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {video && (
+        <video controls width="250" src={URL.createObjectURL(video)}></video>
+      )}
+
+      <input type="file" onChange={(e) => setVideo(e.target.files?.item(0))} />
+
+      <h3>Result</h3>
+      <button onClick={convertToGif}>Convert</button>
+
+      {gif && <img src={gif} />}
     </div>
+  ) : (
+    <p>Loading...</p>
   );
 }
-
-export default App;
